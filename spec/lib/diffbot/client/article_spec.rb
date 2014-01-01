b6337137655a07aaedf7a5237776395cec0f1a5a
Rodolphe Edouard <rodoard@gmail.com>
2014-01-02 01:57:52 +0700

    now actually using fakeweb in tests to fakeweb responses

--- spec/lib/diffbot/client/article_spec.rb
+++ spec/lib/diffbot/client/article_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path("#{File.dirname(__FILE__)}/../../../helpers/article_hel
 describe Diffbot::Clients::Article do
   include Helpers::ArticleHelper
   it "returns article title" do
-    FakeWeb.register_uri(:get, XCONOMY_POST_URL, :response => xconomy_post_json(:title))
+    FakeWeb.register_uri(:get, %r|http://api.diffbot.com/v2/article|, :body => xconomy_post_json(:title).to_json, 'Content-Type' => "application/json")
     Diffbot::Clients::Article.new(DEV_TOKEN, XCONOMY_POST_URL).title.should eql(xconomy_post_title)
   end
 
