6ec1d6a9ddcb6227ec184fe93504e599893bb57d
Rodolphe Edouard <rodoard@gmail.com>
2014-01-02 00:44:30 +0700

    fixed sample test -- returns article title instead of text

--- spec/lib/diffbot/client/article_spec.rb
+++ spec/lib/diffbot/client/article_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path("#{File.dirname(__FILE__)}/../../../helpers/article_hel
 
 describe Diffbot::Clients::Article do
   include Helpers::ArticleHelper
-  it "returns article text" do
+  it "returns article title" do
     FakeWeb.register_uri(:get, XCONOMY_POST_URL, :response => xconomy_post_json(:title))
     Diffbot::Clients::Article.new(DEV_TOKEN, XCONOMY_POST_URL).title.should eql(xconomy_post_title)
   end
